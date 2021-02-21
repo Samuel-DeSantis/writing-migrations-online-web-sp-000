351d9348230a393439f8355199b0b9ef176f2d8b
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-21 05:21:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,13 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
 
-  def up
-  end
-
-  def down
-  end
-
   def change #=> Primary way of writing migrations
-
+    create_table :students do |t|
+      t.string :name
+    end
   end
 
 end
